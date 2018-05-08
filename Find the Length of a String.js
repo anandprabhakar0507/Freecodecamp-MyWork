573dcafaa9144113c6ff704e2eb7846938ee5ceb
Maamar <drx-design@hotmail.com>
2018-05-08 21:10:21 +0100

    Adding my JS result

---
+++ Find the Length of a String.js
@@ -0,0 +1,18 @@
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+//----------------------------------
+// Setup
+var lastNameLength = 0;
+var lastName = "Maamar";
+lastNameLength = lastName.length;
---
+++ Understand String Immutability.js
@@ -0,0 +1,15 @@
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr[0]= "H"; // Fix Me
+
+//Correting it 
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr= "Hello World"; // Fix Me
